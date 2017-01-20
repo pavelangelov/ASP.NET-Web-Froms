13722fc21de7d5392f8da2626c7de28d9ff1b381
pavelangelov <p_a_v_el@abv.bg>
2017-01-20 23:44:44 +0200

    Draw image from text

--- 01.Introduction-to-ASP.NET/01. ASP.NET Web Forms application/CustomHandler.cs
+++ 01.Introduction-to-ASP.NET/01. ASP.NET Web Forms application/CustomHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Web;
 
@@ -15,19 +17,36 @@ public class CustomHandler : IHttpHandler
 
     public void ProcessRequest(HttpContext context)
     {
-        HttpResponse response = context.Response;
-        response.ContentType = "text/plain";
-        response.Write("Request URL: " + context.Request.Url + "\r\n");
-        response.Write("Response date: " + DateTime.Now + "\r\n");
+        context.Response.ContentType = "image/png";
+
+        string text = "";
         if (context.Request.HttpMethod == "GET")
         {
-            response.Write("Request type: GET\r\n");
-            response.Write("Request params: " + context.Request["q"]);
+            text = context.Request["q"];
         }
         else if (context.Request.HttpMethod == "POST")
         {
-            response.Write("Request type: POST\r\n");
-            response.Write("Request params: " + context.Request["postValue"]);
+            text = context.Request["postValue"];
         }
+
+        if (text == null)
+        {
+            text = string.Empty;
+        }
+
+        var path = context.Server.MapPath("App_Data/blank.png");
+        var bitmap = new Bitmap(path);
+
+
+        var graphics = Graphics.FromImage(bitmap);
+        var brush = new SolidBrush(Color.DarkOrange);
+        graphics.FillRectangle(brush, 0, 0, 5000, 150);
+        graphics.DrawString(
+            text,
+            new Font("Bookman Old Style", 12),
+            new SolidBrush(Color.BlanchedAlmond),
+            new PointF(25, 40));
+        context.Response.ContentType = "image/png";
+        bitmap.Save(context.Response.OutputStream, ImageFormat.Png);
     }
 }
