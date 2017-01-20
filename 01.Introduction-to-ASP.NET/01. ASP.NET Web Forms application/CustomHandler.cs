7b4bae62ac75c26b20b294678d70a7329c89afe8
pavelangelov <p_a_v_el@abv.bg>
2017-01-20 22:53:18 +0200

    Custom HTTP Handler

---
+++ 01.Introduction-to-ASP.NET/01. ASP.NET Web Forms application/CustomHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class CustomHandler : IHttpHandler
+{
+    public bool IsReusable
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public void ProcessRequest(HttpContext context)
+    {
+        HttpResponse response = context.Response;
+        response.ContentType = "text/plain";
+        response.Write("Request URL: " + context.Request.Url + "\r\n");
+        response.Write("Response date: " + DateTime.Now + "\r\n");
+        if (context.Request.HttpMethod == "GET")
+        {
+            response.Write("Request type: GET\r\n");
+            response.Write("Request params: " + context.Request["q"]);
+        }
+        else if (context.Request.HttpMethod == "POST")
+        {
+            response.Write("Request type: POST\r\n");
+            response.Write("Request params: " + context.Request["postValue"]);
+        }
+    }
+}
---
+++ 01.Introduction-to-ASP.NET/01. ASP.NET Web Forms application/CustomHttpHandler.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Http Handler" Language="C#" AutoEventWireup="true" CodeBehind="CustomHttpHandler.aspx.cs" Inherits="_01.ASP.NET_Web_Forms_application.CustomHttpHandler" %>
+
+<h2>Custom Http Handler</h2>
+<form action="Sample.img" method="get">
+    <span>Send Get Request: </span>
+    <input type="text"  name="q" />
+    <input type="submit" value="Submit" />
+</form>
+<br />
+<form action="Sample.img" method="post">
+    <span>Send POST Request: </span>
+    <input type="text" name="postValue" value="" />
+    <input type="submit" value="Submit" />
+</form>
+<br />
+<span runat="server" id="result"></span>
